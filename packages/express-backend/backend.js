4eac361dd4972d8bb224742b38907093f6a1c7c3
BPond4 <bpond@calpoly.edu>
2023-10-04 09:55:17 -0700

    search by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,6 +36,20 @@ const users = {
 
 app.use(express.json());
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id); 
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
 const findUserByName = (name) => { 
     return users['users_list']
         .filter( (user) => user['name'] === name); 
